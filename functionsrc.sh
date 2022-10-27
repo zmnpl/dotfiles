923d9b75b7f08c3f2fa9c5a84c73170e8031dcc4
Simon Paul <simonpaul@mailbox.org>
2022-10-27 19:47:36 +0200

    -

--- functionsrc.sh
+++ functionsrc.sh
@@ -20,6 +20,20 @@ meh () {
 	git push
 }
 
+rga-fzf() {
+	RG_PREFIX="rga --files-with-matches"
+	local file
+	file="$(
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+				--phony -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap"
+	)" &&
+	echo "opening $file" &&
+	xdg-open "$file"
+}
+
 plop () {
     open=xdg-open   # this will open pdf file withthe default PDF viewer on KDE, xfce, LXDE and perhaps on other desktops.
 
